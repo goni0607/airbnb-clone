c4f199479b8dc0b2e42596179c230136396cb660
triplek <triplek@triplek.net>
2022-08-18 17:02:32 +0900

    #3.3 Finishing User Model

--- users/models.py
+++ users/models.py
@@ -3,4 +3,29 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    """Custom User Model"""
+
+    GENDER_CHOICE = [("M", "Male"), ("F", "Female"), ("O", "Other")]
+    LANGUAGE_CHOICE = [
+        ("en", "English"),
+        ("ko", "Korean"),
+    ]
+    CURRENCY_CHOICE = [
+        ("usd", "USD"),
+        ("krw", "KRW"),
+    ]
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICE, default="Male", max_length=10, null=True, blank=True
+    )
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICE, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICE, max_length=3, null=True, blank=True
+    )
+    is_superhost = models.BooleanField(default=False, verbose_name="Is Superhost")
+    bio = models.TextField(default="", blank=True)
