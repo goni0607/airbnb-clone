9da82143f9b4c5cb8c0edc61ee79855984bfc950
triplek <triplek@triplek.net>
2022-09-12 21:47:33 +0900

    #10.1 HttpResponse and render

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def all_rooms(request):
-    pass
+    now = datetime.now()
+    return HttpResponse(content=f"<h1>Hello! Now:{now}</h1>")
