97d689e09f99b9ef8be71a9135170cd22b6f355c
triplek <triplek@triplek.net>
2022-09-13 15:15:49 +0900

    #11.8 Class Based Views part Two

--- rooms/views.py
+++ rooms/views.py
@@ -10,6 +10,11 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "-created_at"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 # def all_rooms(request):
