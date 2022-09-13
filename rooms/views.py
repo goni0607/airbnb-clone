ada8c36e1ae7b11511b3b6e9fcdc556dbc30d230
triplek <triplek@triplek.net>
2022-09-13 15:59:35 +0900

    #12.2 room_detail FBV finished

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from . import models
 
 
@@ -19,8 +20,11 @@ class HomeView(ListView):
 
 
 def room_detail(request, pk):
-
-    return render(request, "rooms/room_detail.html")
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/room_detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
 
 
 # def all_rooms(request):
