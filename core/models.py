24c7c73d756c30c5e647071f719d16ebf3aff0a8
triplek <triplek@triplek.net>
2022-09-13 15:02:04 +0900

    #11.7 Class Based Views

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class TimeStampedModel(models.Model):
 
     """Time Stamped Model"""
 
-    created_at = (models.DateTimeField(auto_now_add=True),)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
