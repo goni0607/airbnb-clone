db774d58a697290f9bcc76163582db2030389150
triplek <triplek@triplek.net>
2022-09-14 15:07:37 +0900

    #13.3 Amenities and Facilities Form

--- rooms/views.py
+++ rooms/views.py
@@ -31,10 +31,35 @@ def search(request):
     city = str.capitalize(city)
     country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room_type", 0))
-    room_types = models.RoomType.objects.all()
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
 
-    form = {"s_city": city, "s_room_type": room_type, "s_country": country}
-    choices = {"countries": countries, "room_types": room_types}
+    form = {
+        "s_city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "s_price": price,
+        "s_guests": guests,
+        "s_bedrooms": bedrooms,
+        "s_beds": beds,
+        "s_baths": baths,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+    houserules = models.HouseRule.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+        "houserules": houserules,
+    }
     return render(
         request,
         "rooms/search.html",
