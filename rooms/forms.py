7b2ce3922029d8ba75bc3e24e3c8d00c42085717
triplek <triplek@triplek.net>
2022-09-15 17:15:56 +0900

    #13.9 Forms are Awesome!

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,3 +1,4 @@
+from typing_extensions import Required
 from django import forms
 from django_countries.fields import CountryField
 from . import models
@@ -8,7 +9,7 @@ class SearchForm(forms.Form):
     city = forms.CharField(initial="Anywhere")
     country = CountryField(default="KR").formfield()
     room_type = forms.ModelChoiceField(
-        empty_label="Any Kind", queryset=models.RoomType.objects.all()
+        empty_label="Any Kind", queryset=models.RoomType.objects.all(), required=False
     )
     price = forms.IntegerField(required=False)
     guests = forms.IntegerField(required=False)
@@ -18,11 +19,17 @@ class SearchForm(forms.Form):
     instant_book = forms.BooleanField(required=False)
     is_superhost = forms.BooleanField(required=False)
     amenities = forms.ModelMultipleChoiceField(
-        queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple
+        queryset=models.Amenity.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
     )
     facilities = forms.ModelMultipleChoiceField(
-        queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple
+        queryset=models.Facility.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
     )
     house_rules = forms.ModelMultipleChoiceField(
-        queryset=models.HouseRule.objects.all(), widget=forms.CheckboxSelectMultiple
+        queryset=models.HouseRule.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
     )
