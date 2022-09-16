4e76e9b440057a5740efeb222f7131067b644b61
triplek <triplek@triplek.net>
2022-09-16 14:38:30 +0900

    #15.1 Signup Form part Two

--- users/views.py
+++ users/views.py
@@ -53,10 +53,11 @@ class SignUpView(FormView):
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
 
-    # def form_valid(self, form):
-    #     email = form.cleaned_data.get("email")
-    #     password = form.cleaned_data.get("password")
-    #     user = authenticate(username=email, password=password)
-    #     if user is not None:
-    #         login(self.request, user)
-    #     return super().form_valid(form)
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
