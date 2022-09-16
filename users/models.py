21500e48d31e6a2483c31281bb7d9b3c28bf7f0d
triplek <triplek@triplek.net>
2022-09-16 16:35:33 +0900

    #16.1 Dotenv

--- users/models.py
+++ users/models.py
@@ -21,7 +21,16 @@ class User(AbstractUser):
         choices=GENDER_CHOICE, default="Male", max_length=10, blank=True
     )
     birthdate = models.DateField(null=True, blank=True)
-    language = models.CharField(choices=LANGUAGE_CHOICE, max_length=2, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICE, max_length=3, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICE, max_length=2, blank=True, default="ko"
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICE, max_length=3, blank=True, default="krw"
+    )
     is_superhost = models.BooleanField(default=False, verbose_name="Is Superhost")
     bio = models.TextField(blank=True)
+    email_confirmed = models.BooleanField(default=False)
+    email_secret_key = models.CharField(max_length=120, default="", blank=True)
+
+    def verify_email(self):
+        pass
