9d25ba173b073634d69e55379176d4c3b6d89d67
triplek <triplek@triplek.net>
2022-09-16 14:19:29 +0900

    #15.0 Signup Form

--- users/views.py
+++ users/views.py
@@ -45,3 +45,18 @@ class LogoutView(views.LogoutView):
 # def log_out(request):
 #     logout(request)
 #     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    # def form_valid(self, form):
+    #     email = form.cleaned_data.get("email")
+    #     password = form.cleaned_data.get("password")
+    #     user = authenticate(username=email, password=password)
+    #     if user is not None:
+    #         login(self.request, user)
+    #     return super().form_valid(form)
