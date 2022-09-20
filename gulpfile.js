bc87e21ed45af9629cb0e5825da1cb736fba56f5
triplek <triplek@triplek.net>
2022-09-20 17:41:48 +0900

    #20.1 Header part One

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ const gulp = require("gulp");
 const css = () => {
   const postCSS = require("gulp-postcss");
   //const sass = require("gulp-sass");
-  const sass = require("gulp-sass")(require("node-sass"));
+  const sass = require("gulp-sass")(require("sass"));
   const minify = require("gulp-csso");
-  sass.compiler = require("node-sass");
+  sass.compiler = require("sass");
   return gulp
     .src("assets/scss/styles.scss")
     .pipe(sass().on("error", sass.logError))
