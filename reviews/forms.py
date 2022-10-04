63f7939c0939717db85685efb98c19f38a02e64f
triplek <triplek@triplek.net>
2022-10-04 10:59:53 +0900

    #24.16 Validate Everything

--- reviews/forms.py
+++ reviews/forms.py
@@ -3,6 +3,34 @@ from . import models
 
 
 class CreateReviewForm(forms.ModelForm):
+
+    # accuracy = forms.IntegerField(max_value=5, min_value=1)
+    review = forms.CharField(widget=forms.Textarea)
+    accuracy = forms.ChoiceField(
+        choices=models.Review.RATING_CHOICES,
+        widget=forms.RadioSelect(attrs={"class": "radio-button"}),
+    )
+    communication = forms.ChoiceField(
+        choices=models.Review.RATING_CHOICES,
+        widget=forms.RadioSelect(attrs={"class": "radio-button"}),
+    )
+    cleanliness = forms.ChoiceField(
+        choices=models.Review.RATING_CHOICES,
+        widget=forms.RadioSelect(attrs={"class": "radio-button"}),
+    )
+    location = forms.ChoiceField(
+        choices=models.Review.RATING_CHOICES,
+        widget=forms.RadioSelect(attrs={"class": "radio-button"}),
+    )
+    check_in = forms.ChoiceField(
+        choices=models.Review.RATING_CHOICES,
+        widget=forms.RadioSelect(attrs={"class": "radio-button"}),
+    )
+    value = forms.ChoiceField(
+        choices=models.Review.RATING_CHOICES,
+        widget=forms.RadioSelect(attrs={"class": "radio-button"}),
+    )
+
     class Meta:
         model = models.Review
         fields = (
--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from core import models as core_models
 
 
@@ -6,13 +7,37 @@ class Review(core_models.TimeStampedModel):
 
     """Review Model Definition"""
 
+    RATING_VERY_GOOD = 5
+    RATING_GOOD = 4
+    RATING_AVERAGE = 3
+    RATING_BAD = 2
+    RATING_VERY_BAD = 1
+
+    RATING_CHOICES = (
+        (RATING_VERY_GOOD, "Very Good"),
+        (RATING_GOOD, "Good"),
+        (RATING_AVERAGE, "Average"),
+        (RATING_BAD, "Bad"),
+        (RATING_VERY_BAD, "Very Bad"),
+    )
+
     review = models.CharField(max_length=500)
-    accuracy = models.IntegerField()
-    communication = models.IntegerField()
-    cleanliness = models.IntegerField()
-    location = models.IntegerField()
-    check_in = models.IntegerField()
-    value = models.IntegerField()
+    accuracy = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    communication = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    cleanliness = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    location = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    check_in = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    value = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="reviews"
     )
