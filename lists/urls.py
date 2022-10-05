f98f477baf8c2ce9e29c4af33080b9c1b244510c
triplek <triplek@triplek.net>
2022-10-05 10:57:33 +0900

    #25.6 Fav Page

--- lists/urls.py
+++ lists/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "lists"
 
-urlpatterns = [path("add/<int:room_pk>/", views.add_view, name="add")]
+urlpatterns = [
+    path("toggle/<int:room_pk>/", views.toggle_view, name="toggle"),
+    path("favs/", views.FavListView.as_view(), name="favs"),
+]
--- lists/views.py
+++ lists/views.py
@@ -1,21 +1,32 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.contrib import messages
+from django.views.generic import TemplateView
 from . import models
 from rooms import models as room_models
 
 
-def add_view(request, room_pk):
+def toggle_view(request, room_pk):
     room = room_models.Room.objects.get_or_none(pk=room_pk)
-    if room is not None:
+    action = request.GET.get("action", None)
+    if room is not None and action is not None:
         my_list, _ = models.List.objects.get_or_create(
             name="My Favorite Rooms List", user=request.user
         )
-        list_room = my_list.rooms.get_or_none(pk=room_pk)
-        if list_room is None:
+        if action == "add":
             my_list.rooms.add(room)
-            messages.success(request, "Favorite list added!!")
-        else:
-            messages.warning(request, "This room exists!!")
+        elif action == "remove":
+            my_list.rooms.remove(room)
+        # list_room = my_list.rooms.get_or_none(pk=room_pk)
+        # if list_room is None:
+        #     my_list.rooms.add(room)
+        #     messages.success(request, "Favorite list added!!")
+        # else:
+        #     messages.warning(request, "This room exists!!")
         return redirect(reverse("rooms:detail", kwargs={"pk": room.pk}))
     return redirect(reverse("core:home"))
+
+
+class FavListView(TemplateView):
+
+    template_name = "lists/list_detail.html"
